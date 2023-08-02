ff5d5c91fa0d4d2d769a5e519194d0b263f22d5d
Miytha-alzaabi <139710767+Miytha-alzaabi@users.noreply.github.com>
2023-08-02 15:30:13 +0400

    Add files via upload

---
+++ inherttence/src/Main.java
@@ -0,0 +1,41 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Animal a = new Animal();
+        a.name ="lily";
+        a.color = "white";
+        a.type ="cats";
+        a.speed =10;
+        a.age = 2;
+        System.out.println(a.name);
+        System.out.println(a.color);
+        System.out.println(a.speed);
+        System.out.println(a.age);
+        double s =  a.speedCal();
+        System.out.println(s);
+
+    }
+}
+class Animal extends Cats
+{
+//    String name;
+//    String color;
+    String type;
+
+}
+class Cats
+{
+    String name;
+    String color;
+    double speed;
+    int age;
+    double speedCal()
+    {
+        speed = speed - age;
+        return speed;
+
+    }
+
+}
